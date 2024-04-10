81adbcd0b9a15f7d377b211a5ca3dcba709194d7
imLaeL <isaque.lael@academico.ifpb.edu.br>
2024-04-10 19:57:18 -0300

    ver 0.1

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+
+string = str(input("Digite a string: "))
+
+diretorio_corrente = str(input("Digite o caminho completo do diretório que deseja verificar: "))
+
+arquivos_no_diretorio = [arquivo for arquivo in os.listdir(diretorio_corrente) if os.path.isfile(os.path.join(diretorio_corrente, arquivo))]
+
+for arquivo in arquivos_no_diretorio:
+
+    caminho_completo = os.path.join(diretorio_corrente, arquivo)
+
+    with open(caminho_completo, 'r') as file:
+        conteudo = file.read()
+
+        if string in conteudo:
+            print(f"A palavra '{string}' foi encontrada no arquivo: {caminho_completo}")
